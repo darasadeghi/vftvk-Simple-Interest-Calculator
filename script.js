aaf4bdc14055b1dfaa1ce109be1ffc6688f004c9
Dara Sadeghi <dara.sadeghi99@gmail.com>
2021-08-12 09:13:42 +0000

    update index, script and style

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+//variables for buttons
+   var principal = document.getElementById("principal").value;
+   var rate = document.getElementById("rate").value;
+   var years = document.getElementById("years").value;
+   
+//interest calculation
+   var interest = principal * years * rate/100;
+
+
+
     
 }
         
